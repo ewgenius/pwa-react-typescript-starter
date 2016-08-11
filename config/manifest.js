e525ebd55ca0c6b5c713b72df433a6b4148e46ef
ewgenius <ewgeniux@gmail.com>
2016-08-11 17:46:13 +0300

    init structure

---
+++ config/manifest.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'test app',
+  short_name: 'test app',
+  start_url: '/',
+  scope: '/',
+  display: 'standalone',
+  theme_color: '#fff',
+  background_color: '#fff',
+  icons: [{
+    src: '/assets/icons/icon.png',
+    sizes: '512x512',
+    type: 'image/png'
+  }]
+}
