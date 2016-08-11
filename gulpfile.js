a1feeacdb7cc8b48417ee3ba0c6f6f97584ded32
ewgenius <ewgeniux@gmail.com>
2016-08-11 20:08:07 +0300

    manifest

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,28 @@
 const gulp = require('gulp')
+const pug = require('gulp-pug')
+const file = require('gulp-file')
+
+const manifest = require('./config/manifest')
+
+const BUILD_PATH = './build'
+const SRC_PATH = './src'
+
+gulp.task('pug', () => {
+  return gulp.src(`${SRC_PATH}/index.pug`)
+    .pipe(pug({
+      locals: {
+        title: manifest.name
+      }
+    }))
+    .pipe(gulp.dest(BUILD_PATH))
+})
+
+gulp.task('manifest', () => {
+  return file('manifest.json', JSON.stringify(manifest, null, 2))
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('assets', () => {
+  return gulp.src('./assets/icons/**/*')
+    .pipe(gulp.dest('./build/assets/icons'))
+})
